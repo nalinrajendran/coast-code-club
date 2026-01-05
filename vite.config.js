911ac7da100d3716aa18e5621677a416170599cc
Nalin Rajendran <nalinrajendran1@gmail.com>
2026-01-05 11:57:59 +0530

    Add server configuration to allow specific hosts

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ['coastcodeclub.com']
+  },
   base: '/',
 });
